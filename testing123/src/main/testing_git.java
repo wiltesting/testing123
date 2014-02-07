510db583afea393fd3d62439074102bc9a33e3a8
wilmer234 <hiyip@CSL3WK17.win2k.cs.ust.hk>
2014-02-07 12:14:01 +0800

    asdsadas

---
+++ testing123/src/main/testing_git.java
@@ -0,0 +1,13 @@
+package main;
+
+public class testing_git {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hi");
+	}
+
+}
