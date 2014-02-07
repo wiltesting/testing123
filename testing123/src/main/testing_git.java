a13f94ff0b750558ae86195fe11dbb2c6e6e1df1
wilmer234 <hiyip@CSL3WK17.win2k.cs.ust.hk>
2014-02-07 12:19:30 +0800

    this is second testing

--- testing123/src/main/testing_git.java
+++ testing123/src/main/testing_git.java
@@ -8,6 +8,7 @@ public class testing_git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hi");
+System.out.println("this is second testing");
 	}
 
 }
